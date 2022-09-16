8af56da744b2eddee5b74f86849a8df89ac4358f
Daniil Molodkov <dmolodkov@brainstation.io>
2022-09-16 11:12:20 -0400

    Add simplified API_URL to React app

--- client/src/pages/Warehouse.js
+++ client/src/pages/Warehouse.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import DonutSpinner from "../components/DonutSpinner";
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = '/api';
 
 const Warehouse = () => {
   const [warehouses, setWarehouses] = useState([]);
