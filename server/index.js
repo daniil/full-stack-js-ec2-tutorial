ca852eb17478722e76d48767adf7ba0ad5e35375
Daniil Molodkov <dmolodkov@brainstation.io>
2022-09-16 12:02:59 -0400

    Update Express start script and add additional test endpoint

--- server/index.js
+++ server/index.js
@@ -10,6 +10,9 @@ const inventoryRoutes = require('./routes/inventoryRoute');
 app.get('/api', (_req, res) => {
   res.send("Welcome to my API");
 });
+app.get('/api/test', (_req, res) => {
+  res.json([{ id: 1, item: 'Testing' }]);
+});
 app.use('/api/warehouses', warehouseRoutes);
 app.use('/api/inventories', inventoryRoutes);
 
